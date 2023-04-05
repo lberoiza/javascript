b5c8138e37e6667be2ff3743ddb019f84377a60d
Luis B <luis.beroiza@ottogroup.com>
2023-04-05 12:17:22 +0200

    Agrega configuraciones para Mongoose

--- Backend/index.ts
+++ Backend/index.ts
@@ -7,7 +7,13 @@ let url = 'mongodb://localhost:27017/api_rest_blog';
 let opciones = {
     useNewUrlParser: true
 };
+
+// Opcion invalida
+// mongoose.set('useFindAndModify', false);
+mongoose.Promise = global.Promise;
 mongoose.connect(url, opciones)
 .then(() => {
     console.log("La conexion a la Base de Datos se ha realizado correctamente!!!");
 });
+
+
