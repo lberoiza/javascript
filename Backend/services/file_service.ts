8ea912a9c66b7bfeb9c3930176921aa77b9240ef
Luis B <luis.beroiza@ottogroup.com>
2023-04-08 01:59:37 +0200

    Agrega Servicios para manejar archivos en el servidor

---
+++ Backend/services/file_service.ts
@@ -0,0 +1,51 @@
+import { warn } from 'console';
+import config from '../config/config';
+import { IUploadedFile } from '../models/uploaded_file';
+import fs from 'fs';
+
+
+class FileService {
+
+  // private readonly IMAGE_DIR: string = `${config.RESOURCE_DIR}/images`;
+
+  public async conserveFirstFileAndDeleteRest(fileList: IUploadedFile[], destinationDir: string) : Promise<IUploadedFile>{
+    const firstFile = fileList[0];
+    if(fileList.length > 1) {
+      for( const fileToDelete of fileList.slice(1) ){
+        await this.deleteFile(fileToDelete.path);
+      }
+    }
+
+    await this.moveFile(firstFile, destinationDir);
+    return firstFile;
+  }
+
+
+  public async deleteFile(path: string) {
+    if(path){
+      try {
+        await fs.promises.unlink(path);
+      }catch(error) {
+        console.warn(error);
+      }
+    }
+  }
+
+
+  public async moveFile(fileToMove: IUploadedFile, destinationDir: string) {
+    try {
+      const path: string = `${destinationDir}/${fileToMove.filename}`
+      await fs.promises.rename(fileToMove.path, path);
+      fileToMove.path = path;
+      fileToMove.destination = destinationDir;
+    }catch(error) {
+      console.log(error);
+      throw new Error(`Error al mover el archivo: ${fileToMove.filename}`);
+    }
+  }
+
+
+
+}
+
+export default new FileService();
