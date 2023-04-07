f0e7c096afcaa2f804a24a0f27545d47833701ab
Luis B <luis.beroiza@ottogroup.com>
2023-04-07 21:06:57 +0200

    Usa el archivo de conexion a la base de datos

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,17 +1,20 @@
-import mongoose from 'mongoose';
 import application from './app';
-import express from 'express';
+import config from './config/config'
+import DbConnection from './config/db_connection';
 
-const url: string = 'mongodb://localhost:27017/api_rest_blog';
-const port: number = 3900;
 
-mongoose.connect(url)
-.then(() => {
-  console.log("La conexion a la Base de Datos se ha realizado correctamente!!!");
+async function startServer() {
+  try {
+    await DbConnection.connect();
+    application.listen(config.APP_PORT, () => {
+      console.log(`Servidor corriendo http://localhost:${config.APP_PORT}`);
+    });
+  }catch (error) {
+    console.log(error);
+  }
+}
 
-  application.listen(port, () => {
-    console.log(`Servidor corriendo http://localhost:${3900}`);
-  });
 
+startServer().catch((error) => {
+  console.error('Error al iniciar el servidor:', error);
 });
-
