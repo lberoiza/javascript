9263e03fc358e3e9314154ea26cc685d4e860a2f
Luis B <luis.beroiza@ottogroup.com>
2023-04-08 21:12:15 +0200

    Agrega CORS Personalizado

---
+++ Backend/middleware/custom_cors.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+class CustomCORS {
+
+  public cors(req: Request, res: Response, next: () => void) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  }
+
+}
+
+export default new CustomCORS();
