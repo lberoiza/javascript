2f151c3b88b0aedc0fab299ccb7438ed6021108b
Luis B <luis.beroiza@ottogroup.com>
2023-04-08 13:57:33 +0200

    Agrega Funciones al servicio de imagenes
    - findImageInFileSystem
    - moveImageToResources
    - deleteImagefromResources

--- Backend/services/image_service.ts
+++ Backend/services/image_service.ts
@@ -1,8 +1,14 @@
+import config from "../config/config";
 import { IUploadedFile } from "../models/uploaded_file";
 import imageValidator from "../validations/image_validation";
 import fileService from "./file_service";
+import fs from 'fs';
 
 class ImageService {
+
+  private readonly IMAGE_DIRECTORY = `${config.RESOURCE_DIR}/images`;
+
+
   public conserveFirstImageAndDeleteRest(fileList: IUploadedFile[]) : IUploadedFile{
 
     let selectedImage: IUploadedFile = fileList.find((file) => imageValidator.isValid(file))!;
@@ -14,6 +20,33 @@ class ImageService {
     const filesToDelete = fileList.filter(file => file != selectedImage);
     fileService.deleteFiles(filesToDelete.map(file => file.path));
     return selectedImage;
+  }
+
+
+  public async findImageInFileSystem(fileName: string) : Promise<string> {
+    try {
+      const pathToImage = this.getAbsolutPathToImage(fileName); 
+      await fs.promises.access(pathToImage, fs.constants.F_OK);
+      return pathToImage;
+    }catch (error) {
+      throw new Error(`La imagen '${fileName}' no fue encontrada en el servidor`);
+    }
+  }
+
+
+  public async moveImageToResources(imageFile: IUploadedFile) : Promise<IUploadedFile>{
+    await fileService.moveFile(imageFile, this.IMAGE_DIRECTORY);
+    return imageFile;
+  }
+
+
+  public async deleteImagefromResources(imageName: string) : Promise<void>{
+    await fileService.deleteFile(this.getAbsolutPathToImage(imageName));
+  }
+
+
+  public getAbsolutPathToImage(imageName: string) : string {
+    return `${this.IMAGE_DIRECTORY}/${imageName}`;
   }
 }
 
