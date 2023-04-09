94a6151ae718dbd160462ef516469be6a26adb3b
Luis B <luis.beroiza@ottogroup.com>
2023-04-09 15:13:15 +0200

    Inicio Proyecto React con create-react-app

---
+++ aprendiendo_react/src/components/MiComponente.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MiComponente extends React.Component {
+
+  public render() : JSX.Element {
+    return (
+      <h1>Hola, Soy el componente MiComponente</h1>
+    );
+  }
+}
+
+export default MiComponente;
