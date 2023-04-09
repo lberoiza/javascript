22edee065535d19ce6789ec3de52a5b734023cbb
Luis B <luis.beroiza@ottogroup.com>
2023-04-09 17:46:53 +0200

    Agrega Componente Funcional

--- aprendiendo_react/src/App.tsx
+++ aprendiendo_react/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 
-import MiComponente from './components/MIComponente';
+import MiComponente from './components/MiComponente';
+import MiComponenteFuncional from './components/MiComponenteFuncional';
 
 
 function App() {
@@ -10,17 +11,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <section className="componentes">
+          <MiComponente />
+          <MiComponenteFuncional />
+        </section>
+
       </header>
     </div>
   );
