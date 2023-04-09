57f695fe566ba9f551aaad319d10e9df394c1356
Luis B <luis.beroiza@ottogroup.com>
2023-04-09 23:21:00 +0200

    Agrega Slider

--- aprendiendo_react/src/App.tsx
+++ aprendiendo_react/src/App.tsx
@@ -5,12 +5,14 @@ import './assets/css/App.css';
 // import MiComponente from './components/MiComponente';
 // import MiComponenteFuncional from './components/MiComponenteFuncional';
 import Header from './components/Header';
+import Slider from './components/Slider';
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Slider></Slider>
 
 
       {/* <header className="App-header">
