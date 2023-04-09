d4e0711c4b3a9f97fcfc2ea4d4246a9de574ca62
Luis B <luis.beroiza@ottogroup.com>
2023-04-09 17:46:11 +0200

    Modificando el Componente de Clase

--- aprendiendo_react/src/components/MiComponente.tsx
+++ aprendiendo_react/src/components/MiComponente.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-class MiComponente extends React.Component {
 
+type Receta = {
+  nombre: string,
+  calorias: number,
+  ingredientes: string[]
+};
+
+class MiComponente extends Component {
+
+  private receta : Receta = {
+    nombre: 'Pizza',
+    calorias: 400,
+    ingredientes: ['Tomate', 'Queso', 'Jamon Cocido']
+  }
+
+
+
+
+  // React.Fragment permite poder devolver 2 o mas nodos HTML
+  // sin tener que crear otra etiqueta.
+  // ya que por defecto solo puede devolver un solo node de DOM
   public render() : JSX.Element {
+
     return (
-      <h1>Hola, Soy el componente MiComponente</h1>
+      <Fragment>
+        <h1>Receta: {this.receta.nombre}</h1>
+        <h3>Calorias: {this.receta.calorias}</h3>
+        <ol>
+        {this.receta.ingredientes.map((ingrediente, index) => (<li key={index}>{ingrediente}</li>))}
+        </ol>
+      </Fragment>
+
     );
   }
 }
