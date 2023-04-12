a8103526f661a596822f95301042009a615f0d52
Luis B <luis.beroiza@ottogroup.com>
2023-04-12 22:08:45 +0200

    Cambia apariencia del SideBar.
    Si está en la pagina del blog aparece la opcion de crear articulo.

--- proyecto_react_vite/src/components/Sidebar.tsx
+++ proyecto_react_vite/src/components/Sidebar.tsx
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 
-class Sidebar extends Component {
+
+export type SidebarProps = {
+  isBlog?: boolean
+};
+
+class Sidebar extends Component<SidebarProps> {
+
+
+
+  private getNavBlog() : JSX.Element | null{
+    const navBlog = this.props.isBlog ?
+    (
+      <div id="nav-blog" className="sidebar-item">
+        <h3>Puedes hacer esto</h3>
+        <a href="" className="btn btn-success">Crear Articulo</a>
+      </div>
+    ) : null;
+    return navBlog;
+  }
+
 
   public render(): JSX.Element {
     return (
       <aside id="sidebar">
-        <div id="nav-blog" className="sidebar-item">
-          <h3>Puedes hacer esto</h3>
-          <a href="" className="btn btn-success">Crear Articulo</a>
-        </div>
+        {this.getNavBlog()}
 
         <div id="search" className="sidebar-item">
           <h3>Buscador</h3>
