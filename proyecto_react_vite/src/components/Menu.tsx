e986bdb59db1c62db3a05db4d92229b26ad05e4c
Luis B <luis.beroiza@ottogroup.com>
2023-04-12 15:52:53 +0200

    Agrega una Linea debajo del menu activo

--- proyecto_react_vite/src/components/Menu.tsx
+++ proyecto_react_vite/src/components/Menu.tsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
+
+
 class Menu extends Component {
+
+  classNameMethod(arg: {isActive: boolean, isPending: boolean }) : string {
+    return arg.isActive ? "navlink_active" : ""
+  }
+  
+
 
   public render(): JSX.Element {
     return (
       < nav id="menu" >
         <ul>
-          <li><NavLink to="/">Inicio</NavLink></li>
-          <li><NavLink to="blog">Blog</NavLink></li>
-          <li><NavLink to="formulario">Formulario</NavLink></li>
-          <li><NavLink to="pagina1">Pagina 1</NavLink></li>
-          <li><NavLink to="pagina2">Pagina 2</NavLink></li>
+          <li><NavLink className={this.classNameMethod} to="/">Inicio</NavLink></li>
+          <li><NavLink className={this.classNameMethod} to="blog">Blog</NavLink></li>
+          <li><NavLink className={this.classNameMethod} to="formulario">Formulario</NavLink></li>
+          <li><NavLink className={this.classNameMethod} to="pagina1">Pagina 1</NavLink></li>
+          <li><NavLink className={this.classNameMethod} to="pagina2">Pagina 2</NavLink></li>
         </ul>
       </nav >
     );
