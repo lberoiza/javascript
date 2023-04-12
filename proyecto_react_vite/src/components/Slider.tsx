553ede8f289c36d9d0d70d597bc2e8bd7e918e8f
Luis B <luis.beroiza@ottogroup.com>
2023-04-12 22:09:42 +0200

    Cambia apariencia del Slider dependiendo si está en la pagina del Blog
    Si está en el blog se Muestra mas diminuto y sin boton para ir al blog

--- proyecto_react_vite/src/components/Slider.tsx
+++ proyecto_react_vite/src/components/Slider.tsx
@@ -1,9 +1,18 @@
-function Slider() : JSX.Element {
+import { NavLink } from "react-router-dom";
+
+export type SliderProps = {
+  isBlog?: boolean
+};
+
+function Slider(props: SliderProps): JSX.Element {
+  const className = props.isBlog ? "slider-small" : "slider-big";
+
   return (
-    <div id="slider" className="slider-big">
-    <h1>Bienvenido al master der ReactJS</h1>
-    <a href="blog.html" className="btn-white">Ir al blog</a>
-  </div>
+    <div id="slider" className={className}>
+      <h1>Bienvenido al master der ReactJS</h1>
+      {!props.isBlog && <NavLink to="/blog" className="btn-white">Ir al blog</NavLink>}
+    
+    </div>
   );
 }
 
