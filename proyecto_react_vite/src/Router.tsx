9a937805cdc8064ce2d9e22909d882586fc9e794
Luis B <luis.beroiza@ottogroup.com>
2023-04-12 22:12:02 +0200

    Agrega Paginas al Router Home, Blog

--- proyecto_react_vite/src/Router.tsx
+++ proyecto_react_vite/src/Router.tsx
@@ -3,12 +3,11 @@ import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 
 import Header from './components/Header';
-import Slider from './components/Slider';
-import Sidebar from './components/Sidebar';
+import Home from './Pages/Home';
+import Blog from './Pages/Blog'
 import ClearFix from './components/ClearFix';
 import Footer from './components/Footer';
 
-import UltimosArticulos from './components/UltimosArticulos';
 import MiComponente from './components/ejemplos/MiComponente';
 import NotFound from './components/NotFound';
 
@@ -20,24 +19,18 @@ class Router extends Component {
     return (
       <BrowserRouter>
         <Header></Header>
-        <Slider></Slider>
-        <div className="center">
-          <section id="content">
-            <Routes>
-              <Route path="/" Component={UltimosArticulos} />
-              <Route path="/blog" Component={MiComponente} />
-              <Route path="/formulario" element={
-                <>
-                  <h2>Poniendo texto desde un render en una ruta</h2>
-                  <p>Este es otro elemento JSX</p>
-                </>
-              } />
-              <Route path='*' Component={NotFound}></Route>
-            </Routes>
-          </section>
-          <Sidebar></Sidebar>
-          <ClearFix></ClearFix>
-        </div>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/blog" Component={Blog} />
+          <Route path="/formulario" element={
+            <>
+              <h2>Poniendo texto desde un render en una ruta</h2>
+              <p>Este es otro elemento JSX</p>
+            </>
+          } />
+          <Route path='*' Component={NotFound}></Route>
+        </Routes>
+        <ClearFix></ClearFix>
 
         <Footer></Footer>
       </BrowserRouter>
