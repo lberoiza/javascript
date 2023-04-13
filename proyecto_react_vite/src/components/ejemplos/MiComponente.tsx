6090a6b577ee982ecfcbbeb28e32353c5c03f417
Luis B <luis.beroiza@ottogroup.com>
2023-04-13 15:21:07 +0200

    Formateando Archivo

--- proyecto_react_vite/src/components/ejemplos/MiComponente.tsx
+++ proyecto_react_vite/src/components/ejemplos/MiComponente.tsx
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from "react";
 
-
 type Receta = {
   nombre: string,
   calorias: number,
@@ -9,26 +8,25 @@ type Receta = {
 
 class MiComponente extends Component {
 
-  private receta : Receta = {
+  private receta: Receta = {
     nombre: 'Pizza',
     calorias: 400,
     ingredientes: ['Tomate', 'Queso', 'Jamon Cocido']
   }
 
 
-
-
   // React.Fragment permite poder devolver 2 o mas nodos HTML
   // sin tener que crear otra etiqueta.
   // ya que por defecto solo puede devolver un solo node de DOM
-  public render() : JSX.Element {
+  public render(): JSX.Element {
 
     return (
       <Fragment>
         <h1>Receta: {this.receta.nombre}</h1>
+        {/* {tituloUrl} */}
         <h3>Calorias: {this.receta.calorias}</h3>
         <ol>
-        {this.receta.ingredientes.map((ingrediente, index) => (<li key={index}>{ingrediente}</li>))}
+          {this.receta.ingredientes.map((ingrediente, index) => (<li key={index}>{ingrediente}</li>))}
         </ol>
       </Fragment>
 
