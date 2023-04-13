87f6f350a6cb3c1e60e6172246c1996d45d487fc
Luis B <luis.beroiza@ottogroup.com>
2023-04-13 15:59:01 +0200

    Manejo de Formulario

--- proyecto_react_vite/src/Pages/Formulario.tsx
+++ proyecto_react_vite/src/Pages/Formulario.tsx
@@ -3,8 +3,28 @@ import React, { Component } from 'react';
 import Slider from '../components/Slider';
 import Sidebar from '../components/Sidebar';
 import UltimosArticulos from '../components/UltimosArticulos';
+import { on } from 'events';
 
 class Formulario extends Component {
+
+  private formAttrib = {
+    nombre: React.createRef<HTMLInputElement>(),
+    apellido: React.createRef<HTMLInputElement>(),
+    biografia: React.createRef<HTMLTextAreaElement>(),
+    genero: {
+      hombre: React.createRef<HTMLInputElement>(),
+      mujer: React.createRef<HTMLInputElement>(),
+      noBinario: React.createRef<HTMLInputElement>()
+    },
+  };
+
+  
+  private onSubmitMethode = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log(this.formAttrib);
+    console.log(this.formAttrib.nombre.current?.value);
+    event.preventDefault();
+  };
+
 
   public render(): JSX.Element {
     return (
@@ -13,28 +33,28 @@ class Formulario extends Component {
         <div className="center">
           <section id="content">
 
-            <form className="mid-form">
+            <form className="mid-form" onSubmit={this.onSubmitMethode}>
               <div className="form-group">
                 <label htmlFor="nombre">Nombre</label>
-                <input type="text" name="nombre" />
+                <input type="text" name="nombre" ref={this.formAttrib.nombre} />
               </div>
 
               <div className="form-group">
                 <label htmlFor="apellido">Apellido</label>
-                <input type="text" name="apellido" />
+                <input type="text" name="apellido" ref={this.formAttrib.apellido} />
               </div>
 
 
               <div className="form-group">
                 <label htmlFor="biografia">Biografía</label>
-                <textarea name="biografia" cols={30} rows={10}></textarea>
+                <textarea name="biografia" cols={30} rows={10} ref={this.formAttrib.biografia} ></textarea>
               </div>
 
 
               <div className="form-group radio-button">
-                <input type="radio" name="genero" /> Hombre
-                <input type="radio" name="genero" /> Mujer
-                <input type="radio" name="genero" /> No Binario
+                <input type="radio" name="genero" ref={this.formAttrib.genero.hombre} /> Hombre
+                <input type="radio" name="genero" ref={this.formAttrib.genero.mujer} /> Mujer
+                <input type="radio" name="genero" ref={this.formAttrib.genero.noBinario} /> No Binario
               </div>
 
               <div className="clearfix"></div>
