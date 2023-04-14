dc2badea3ec8b8f91bb2e79b1001ece13c50c157
Luis B <luis.beroiza@ottogroup.com>
2023-04-14 23:19:51 +0200

    Agrega Imagen por Defautl cuando articulo no tiene imagen

--- proyecto_react_vite/src/api/ApiImage.ts
+++ proyecto_react_vite/src/api/ApiImage.ts
@@ -1,10 +1,11 @@
 import Config from "../config/Config";
+import NoImage from "../assets/images/no_image.png"
 
 class ApiImage {
   private readonly URL_IMAGE_LIST = `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/image`;
 
   public getImageUrl(imageName: string): string {
-    return imageName? `${this.URL_IMAGE_LIST}/${imageName}` : '';
+    return imageName? `${this.URL_IMAGE_LIST}/${imageName}` : NoImage;
   }
 }
 
