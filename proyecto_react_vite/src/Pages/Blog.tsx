c1226d378ae6e75569d54dba7141b91fe3faaaaf
Luis B <luis.beroiza@ottogroup.com>
2023-04-14 14:29:02 +0200

    Muestra Datos de Articulos en el Blog
    Los datos son leidos desde el Backend y cargados en el Frontend.

--- proyecto_react_vite/src/Pages/Blog.tsx
+++ proyecto_react_vite/src/Pages/Blog.tsx
@@ -3,30 +3,73 @@ import React, { Component } from 'react';
 import Slider from '../components/Slider';
 import Sidebar from '../components/Sidebar';
 import MiComponente from '../components/ejemplos/MiComponente';
-import FetchRequest, { WsResponse } from '../classes/FetchRequest';
+import ApiArticle from '../api/ApiArticle';
+import WsResponse, { WsResponseObject } from '../classes/WsResponse';
+import { FetchRequestResponse } from '../classes/FetchRequest';
 
 
-type apiArticles = {
+
+
+type BlogPropsType = {
+  [key: string]: string
+}
+
+type BlogStateTyp = {
   articles: any
 };
 
 
-class Blog extends Component<{}, apiArticles> {
+class Blog extends Component<BlogPropsType, WsResponseObject> {
 
-  state: apiArticles = { articles: {} };
+  private wsResponse = new WsResponse()
+  state: WsResponseObject = this.wsResponse.toState();
+
+  constructor(props: BlogPropsType) {
+    super(props);
+  }
+
+  componentDidMount(): void {
+    this.loadArticles();
+  }
+
+  private loadArticles(): void {
+    const self = this;
+    ApiArticle.getList().then((response: FetchRequestResponse) => {
+      console.log(response);
+      self.setState(self.wsResponse.setFetchRequestResponse(response).toState());
+    }).catch(error => {
+      console.error(error)
+      self.wsResponse.addError("Hubo un error al cargar los datos");
+      self.setState(self.wsResponse.toState());
+    });
+  }
 
 
-  private showArticles = (wsResponse: WsResponse) => {
-    console.log(wsResponse);
+
+  private showArticles(): JSX.Element {
+    if (this.wsResponse.isPending()) {
+      return (<p>Cargando articulos...</p>);
+    } else {
+      if (this.wsResponse.hasResponse()) {
+        return (
+          <p>Se encontraron un total de {this.wsResponse.getResponse().length}</p>
+        );
+      } else {
+        return (
+            <p>Todavia no existen articulos para mostar</p>
+          );
+      }
+
+    }
+
   }
 
   public render(): JSX.Element {
-
-    // FetchRequest.get('http://localhost:3900/api/article/all/5', this.showArticles);
     return (
       <div id="blog">
         <Slider title='Blog'></Slider>
         <div className="center">
+          {this.showArticles()}
           <section id="content">
             {/* Aqui se cargaran mediante api rest, la lista de articulos */}
             <MiComponente />
