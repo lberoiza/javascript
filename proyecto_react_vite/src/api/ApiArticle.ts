b2bcab210f3f7738c57f1f664788449176299aa1
Luis B <luis.beroiza@ottogroup.com>
2023-04-14 18:07:50 +0200

    Agrega Config a ApiArticle

--- proyecto_react_vite/src/api/ApiArticle.ts
+++ proyecto_react_vite/src/api/ApiArticle.ts
@@ -1,3 +1,4 @@
+import Config from '../config/Config'
 import FetchRequest from "../classes/FetchRequest";
 import { FetchRequestResponse } from "../classes/FetchRequest";
 
@@ -8,7 +9,7 @@ export type ArticleTyp = {
 
 class ApiArticle {
 
-  private readonly URL_ARTICLE_LIST = "http://localhost:3900/api/article/all";
+  private readonly URL_ARTICLE_LIST = `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/all`
 
   public getList() : Promise<FetchRequestResponse>{
     return FetchRequest.get(this.URL_ARTICLE_LIST)
