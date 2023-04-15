4d630d76ce43a033f339c63c1ab6326072f4787a
Luis B <luis.beroiza@ottogroup.com>
2023-04-15 22:43:03 +0200

    Agregando Lenguaje por defecto para la App

--- proyecto_react_vite/src/config/Config.ts
+++ proyecto_react_vite/src/config/Config.ts
@@ -2,13 +2,15 @@ interface ConfigProps {
   readonly APP_PORT: number;
   readonly APP_API_PREFIX: string;
   readonly APP_API_HOST: string;
+  readonly DEFAULT_LANGUAGE: string;
   //...otras variables de configuración
 }
 
 const Config: ConfigProps = {
   APP_API_HOST: 'localhost',
   APP_PORT: 3900,
-  APP_API_PREFIX: '/api'
+  APP_API_PREFIX: '/api',
+  DEFAULT_LANGUAGE: 'es'
 };
 
 export default Config;
