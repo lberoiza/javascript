b6e500b3a13b7ac7662109bfd3b72b4a8f6eee00
Luis B <luis.beroiza@ottogroup.com>
2023-04-15 22:44:09 +0200

    Agrega Componente Dayjs
    <Dayjs></Dayjs> funciona como un <time></time>
    pero con la fecha formateada

---
+++ proyecto_react_vite/src/components/Dayjs.tsx
@@ -0,0 +1,27 @@
+import Config from '../config/Config';
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/es';
+import 'dayjs/locale/en';
+import 'dayjs/locale/de';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+
+interface TimeProps extends React.HTMLAttributes<HTMLTimeElement> {
+  locale?: string;
+  format?: string;
+}
+
+const Dayjs: React.FC<TimeProps> = ({ locale = `${Config.DEFAULT_LANGUAGE}`, ...props }) => {
+  const datetime = new Date(props.children!.toString());
+  const dayJsDate = dayjs(datetime).locale(locale);
+  
+  const formatedDate = props.format? dayJsDate.format(props.format) : dayJsDate.fromNow();
+  
+  return <time dateTime={datetime.getTime().toString()} {...props}>{formatedDate}</time>;
+};
+
+export default Dayjs;
+
+
