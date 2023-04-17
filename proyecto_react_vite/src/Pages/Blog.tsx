38b5e8305f39685c308963e1beca1f59e7ec284e
Luis B <luis.beroiza@ottogroup.com>
2023-04-17 22:11:44 +0200

    Usa useFetch en el Blog y Convierte el Componente a Funcional

--- proyecto_react_vite/src/Pages/Blog.tsx
+++ proyecto_react_vite/src/Pages/Blog.tsx
@@ -1,106 +1,59 @@
-import React, { Component } from 'react';
-
-import Slider from '../components/Slider';
-import Sidebar from '../components/Sidebar';
-import ApiArticle from '../api/ApiArticle';
-import WsResult, { WsResultObject } from '../classes/WsResult';
-import { FetchRequestResponse } from '../classes/FetchRequest';
-import ArticlePreview, { ArticlePreviewProps } from '../components/ArticlePreview';
-
-
-type BlogPropsType = {
-  [key: string]: string
-}
+import { useState } from "react";
+import ApiConstant from "../api/ApiConstant";
+import Sidebar from "../components/Sidebar";
+import Slider from "../components/Slider";
+import useFetch from "../custom_hooks/useFetch";
+import { ArticleResponse } from "../api/ApiArticle";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import ArticlePreview from "../components/ArticlePreview";
 
 type BlogState = {
-  followArticleTitle: string,
-  articleRespose: WsResultObject
+  followArticleTitle: string
 }
 
-class Blog extends Component<BlogPropsType, BlogState> {
 
-  private wsResult = new WsResult()
-  state: BlogState = {
-    followArticleTitle: '',
-    articleRespose: this.wsResult.toState()
+function showArticleList(articleList: ArticleResponse[] | null, followArticle: (f: ArticleResponse) => void): JSX.Element {
+  if (articleList) {
+    return (
+      <>
+        {articleList.map(article => (<ArticlePreview key={article._id} {...article} follow={followArticle} />))}
+      </>
+    );
+  } else {
+    return (
+      <p>Todavia no existen articulos para mostar</p>
+    );
+  }
+}
+
+
+const Blog = (): JSX.Element => {
+
+  const { data, error, loading } = useFetch<ArticleResponse[]>(ApiConstant.ARTICLE.GET_ALL_ARTICLES);
+  const [blogState, setBlogState] = useState<BlogState>({
+    followArticleTitle: ''
+  });
+
+  const followArticle = (article: ArticleResponse): void => {
+    setBlogState({ followArticleTitle: article.title });
   };
 
-  constructor(props: BlogPropsType) {
-    super(props);
-  }
-
-  componentDidMount(): void {
-    this.loadArticles();
-  }
-
-  private loadArticles(): void {
-    const self = this;
-    ApiArticle.getList().then((response: FetchRequestResponse) => {
-      self.setState({ articleRespose: self.wsResult.setFetchRequestResponse(response).toState() });
-    }).catch(error => {
-      console.error(error)
-      self.wsResult.addError("Hubo un error al cargar los datos");
-      self.setState({ articleRespose: self.wsResult.toState() });
-    });
-  }
-
-
-
-  private showArticles(): JSX.Element {
-    if (this.wsResult.isPending()) {
-      return this.showLoading();
-    } else {
-      return this.showArticleList();
-
-    }
-  }
-
-
-  private showLoading(): JSX.Element {
-    return (
-      <p>Cargando articulos...</p>
-    );
-  }
-
-
-  private showArticleList(): JSX.Element {
-    if (this.wsResult.hasResponse()) {
-      const articleList = this.wsResult.getResponse() as ArticlePreviewProps[]
-      return (
-        <>
-          {articleList.map(article => (<ArticlePreview key={article._id} {...article} follow={this.followArticle} />))}
-        </>
-      );
-    } else {
-      return (
-        <p>Todavia no existen articulos para mostar</p>
-      );
-    }
-  }
-
-
-  private followArticle = (article: ArticlePreviewProps): void => {
-    this.setState({ followArticleTitle: article.title });
-  }
-
-  public render(): JSX.Element {
-    return (
-      <div id="blog">
-        <Slider title='Blog'></Slider>
-        <div className="center">
-          <section id="content">
-            {/* Aqui se cargaran mediante api rest, la lista de articulos */}
-            {/* Operador ternario compacto. si this.state.followArticleTitle existe, se renderiza el resto*/}
-            {this.state.followArticleTitle && <div className="message">{this.state.followArticleTitle}</div>}
-            {this.showArticles()}
-            {/* <MiComponente /> */}
-          </section>
-          <Sidebar isBlog></Sidebar>
-        </div>
+  return (
+    <div id="blog">
+      <Slider title='Blog'></Slider>
+      <div className="center">
+        <section id="content">
+          {/* Aqui se cargaran mediante api rest, la lista de articulos */}
+          {blogState.followArticleTitle && <div className="message">{blogState.followArticleTitle}</div>}
+          {loading && <Loading></Loading>}
+          {error && <Error></Error>}
+          {data.response && showArticleList(data.response, followArticle)}
+        </section>
+        <Sidebar isBlog></Sidebar>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 
-export default Blog;
+export default Blog
