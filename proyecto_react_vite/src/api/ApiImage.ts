649dd60db35daf0343dadd5789dfe99a81ef817b
Luis B <luis.beroiza@ottogroup.com>
2023-04-17 18:37:25 +0200

    Muestra Imagen por defecto, cuando el articulo no tiene imagen.

--- proyecto_react_vite/src/api/ApiImage.ts
+++ proyecto_react_vite/src/api/ApiImage.ts
@@ -4,7 +4,7 @@ import NoImage from "../assets/images/no_image.png"
 class ApiImage {
   private readonly URL_IMAGE_LIST = `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/image`;
 
-  public getImageUrl(imageName: string): string {
+  public getImageUrl(imageName: string | null): string {
     return imageName? `${this.URL_IMAGE_LIST}/${imageName}` : NoImage;
   }
 }
