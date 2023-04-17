83bfd271bcaf9543c226244bd627bc8584eb8a25
Luis B <luis.beroiza@ottogroup.com>
2023-04-17 18:39:46 +0200

    Agrega Componente para mostar que los datos están siendo cargados.

---
+++ proyecto_react_vite/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+const Loading = function (): JSX.Element {
+  return (
+    <>
+      <h2 className="subheader">Cargando los datos...</h2>
+      <p>Por favor espere mientras se cargan los datos.</p>
+    </>
+  );
+}
+
+export default Loading;
