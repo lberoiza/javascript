b360dec98c564d3ea9139d1eb3762fd579c26ac7
Luis B <luis.beroiza@ottogroup.com>
2023-04-17 18:36:11 +0200

    Agrega Pagina con mensaje NotFound

--- proyecto_react_vite/src/Pages/NotFound.tsx
+++ proyecto_react_vite/src/Pages/NotFound.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const NotFound = function () : JSX.Element {
   return (
-    <React.Fragment>
+    <>
       <h2 className="subheader">Pagina no encontrada</h2>
       <p>La pagina a la que intentas acceder no existe.</p>
-    </React.Fragment>
+    </>
   );
 }
 
