3674643f79ced4fdbfcddf3df651c1fca016050e
Luis B <luis.beroiza@ottogroup.com>
2023-04-17 20:28:12 +0200

    Agrega componente que muestra la pagina de un articulo completo

--- proyecto_react_vite/src/Pages/Article.tsx
+++ proyecto_react_vite/src/Pages/Article.tsx
@@ -1,71 +1,51 @@
-import ClearFix from "../components/ClearFix";
+import ApiConstant from "../api/ApiConstant";
 import Sidebar from "../components/Sidebar";
 import { useParams } from "react-router-dom";
+import useFetch from "../custom_hooks/useFetch";
+import NotFound from "./NotFound";
+import Loading from "../components/Loading";
+import Dayjs from "../components/Dayjs";
+import ApiImage from "../api/ApiImage";
+import Error from "../components/Error";
+import {ArticleResponse} from "../api/ApiArticle";
 
 type ArticleUrlParams = {
   id: string;
 }
 
+function renderArticle(response: ArticleResponse): JSX.Element {
+  return (
+    <article className="article-item article-detail">
+      <div className="image-wrap">
+        <img src={ApiImage.getImageUrl(response.image)} alt="Article Image" />
+      </div>
+      <h1 className="subheader">{response.title}</h1>
+      <Dayjs className="date">{response.date}</Dayjs>
+      <div className="article-text">
+        {response.content}
+      </div>
+    </article>
+  );
+}
 
-const Article = (props: any): JSX.Element => {
+
+const Article = (): JSX.Element => {
   const { id } = useParams<ArticleUrlParams>();
+  if (id === undefined) return <NotFound></NotFound>
+
+  const { data, loading, error } = useFetch<ArticleResponse>(`${ApiConstant.ARTICLE.GET_BY_ID}/${id}`);
+
   return (
     <div className="center">
-    <section id="content">
-      <div id="article-container">
-        <article className="article-item article-detail">
-            <div className="image-wrap">
-              <img src="https://picsum.photos/1000/800" alt="Article Image" />
-            </div>
-            <h1 className="subheader">Articulo de prueba</h1>
-            <span className="date">
-              hace 5 min
-            </span>
-            <div className="article-text">
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua.
-                At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua.
-                At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua.
-                At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua.  
-              </p>
-            </div>
-        </article>
-      </div>
-    </section>
-
-    <Sidebar isBlog></Sidebar>
-    <ClearFix></ClearFix>
-  </div>
+      <section id="content">
+        <div id="article-container">
+          {loading && <Loading></Loading>}
+          {error && <Error></Error>}
+          {data.response && renderArticle(data.response)}
+        </div>
+      </section>
+      <Sidebar isBlog></Sidebar>
+    </div>
   );
 }
 
