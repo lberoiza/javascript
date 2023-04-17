42504de910be85201ae1443d4bbafda4ccb20981
Luis B <luis.beroiza@ottogroup.com>
2023-04-17 22:07:31 +0200

    Cambia Type por Interface, para que pueda ser heredada

--- proyecto_react_vite/src/api/ApiArticle.ts
+++ proyecto_react_vite/src/api/ApiArticle.ts
@@ -2,25 +2,11 @@ import Config from '../config/Config'
 import FetchRequest from "../classes/FetchRequest";
 import { FetchRequestResponse } from "../classes/FetchRequest";
 
-export type ArticleResponse = {
-  __id: string,
+export interface ArticleResponse {
+  _id: string,
   title: string,
   content: string,
   date: string,
   image: string,
   __v: number
 }
-
-
-class ApiArticle {
-
-  private readonly URL_ARTICLE_LIST = `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/all`
-
-  public getList() : Promise<FetchRequestResponse>{
-    return FetchRequest.get(this.URL_ARTICLE_LIST)
-  }
-
-}
-
-
-export default new ApiArticle();
