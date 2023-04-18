f5e197fd55c464e3fe4a2c0e87a8f7b3cbdf1ce4
Luis B <luis.beroiza@ottogroup.com>
2023-04-18 15:26:59 +0200

    Agrega Pagina para Mostrar resultados de busqueda de articulos

---
+++ proyecto_react_vite/src/Pages/Search.tsx
@@ -0,0 +1,23 @@
+import ArticleListSearch from "../components/ArticleListSearch";
+import Sidebar from "../components/Sidebar";
+import Slider from "../components/Slider";
+import { useParams } from "react-router-dom";
+
+
+const Search = (): JSX.Element => {
+  const { search } = useParams<string>();
+
+  return (
+    <div id="search">
+      <Slider title={"Busqueda: "+search}></Slider>
+      <div className="center">
+        <section id="content">
+          <ArticleListSearch search={search!}></ArticleListSearch>
+        </section>
+        <Sidebar isBlog></Sidebar>
+      </div>
+    </div>
+  );
+}
+
+export default Search
