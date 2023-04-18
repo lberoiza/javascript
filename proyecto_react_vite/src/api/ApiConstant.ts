4c7ac8d16226a6dac2f5634cb887358d91e33d62
Luis B <luis.beroiza@ottogroup.com>
2023-04-18 15:26:05 +0200

    Agrega Direccion URL para realizar busqueda de Articulos

--- proyecto_react_vite/src/api/ApiConstant.ts
+++ proyecto_react_vite/src/api/ApiConstant.ts
@@ -3,7 +3,8 @@ import Config from "../config/Config";
 type ApiConstantProps = {
   readonly ARTICLE: {
     readonly GET_BY_ID: string,
-    readonly GET_ALL_ARTICLES: string
+    readonly GET_ALL_ARTICLES: string,
+    readonly GET_ALL_BY_SEARCH: string
   },
   readonly IMAGE: {
     readonly GET_IMAGE_BY_NAME: string
@@ -14,7 +15,8 @@ type ApiConstantProps = {
 const ApiConstant: ApiConstantProps = {
   ARTICLE: {
     GET_BY_ID: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article`,
-    GET_ALL_ARTICLES: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/all`
+    GET_ALL_ARTICLES: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/all`,
+    GET_ALL_BY_SEARCH: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/search`
   },
   IMAGE: {
     GET_IMAGE_BY_NAME: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/image`
