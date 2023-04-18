d037e38ab657faf2843293292cce2bfa4d6644e9
Luis B <luis.beroiza@ottogroup.com>
2023-04-18 21:19:19 +0200

    Convierte Sidebar Componente a componente Funcional
    Ademas se separo 2 Componentes:
     - ArticleNew: Boton para crear articulos nuevos
     - SearchArticleForm: Formulario de busqueda de Articulos
    
    Ambos componentes se usan acá

--- proyecto_react_vite/src/components/Sidebar.tsx
+++ proyecto_react_vite/src/components/Sidebar.tsx
@@ -1,42 +1,19 @@
-import React, { Component } from "react";
+import SearchArticleForm from "./SearchArticleForm";
+import ArticleNew from "./ArticleNew";
 
 
 export type SidebarProps = {
   isBlog?: boolean
 };
 
-class Sidebar extends Component<SidebarProps> {
 
-
-
-  private showNavBlogIfPageIsBlog() : JSX.Element | null{
-    const navBlog = this.props.isBlog ?
-    (
-      <div id="nav-blog" className="sidebar-item">
-        <h3>Puedes hacer esto</h3>
-        <a href="" className="btn btn-success">Crear Articulo</a>
-      </div>
-    ) : null;
-    return navBlog;
-  }
-
-
-  public render(): JSX.Element {
-    return (
-      <aside id="sidebar">
-        {this.showNavBlogIfPageIsBlog()}
-
-        <div id="search" className="sidebar-item">
-          <h3>Buscador</h3>
-          <p>Encuentra el articulo que buscas</p>
-          <form action="">
-            <input type="text" name="search" id="search-input-text" />
-            <input type="submit" value="Buscar" name="submit" id="search-btn-submit" className="btn" />
-          </form>
-        </div>
-      </aside>
-    );
-  }
+const Sidebar = (props: SidebarProps): JSX.Element => {
+  return (
+    <aside id="sidebar">
+      {props.isBlog && <ArticleNew />}
+      <SearchArticleForm></SearchArticleForm>
+    </aside>
+  );
 }
 
 export default Sidebar;
