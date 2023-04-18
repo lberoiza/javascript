a55270071266b01feba9f8b959378a654ce13fb7
Luis B <luis.beroiza@ottogroup.com>
2023-04-18 12:19:45 +0200

    Usa FetchData como respuesta del Custom Hook useFetch

--- proyecto_react_vite/src/custom_hooks/useFetch.ts
+++ proyecto_react_vite/src/custom_hooks/useFetch.ts
@@ -1,30 +1,17 @@
 import { useState, useEffect } from "react";
-
-export type FetchResponse<T> = {
-  isSuccessful: boolean,
-  successMessages: string[],
-  warningMessages: string[],
-  errorMessages: string[],
-  response: T | null,
-};
+import UseFetchData, { IUseFetchData } from "../classes/UseFetchData";
 
 export type UseFetchReturn<T> = {
-  data: FetchResponse<T>;
+  data: UseFetchData<T>;
   loading: boolean;
   error: Error | null;
   handleCancelRequest: () => void;
 }
 
 const useFetch = <T>(url: string): UseFetchReturn<T> => {
-  const fetchResponse = <FetchResponse<T>>({
-    isSuccessful: true,
-    successMessages: [],
-    warningMessages: [],
-    errorMessages: [],
-    response: null,
-  });
+  const fetchData = new UseFetchData<T>();
 
-  const [data, setData] = useState<FetchResponse<T>>(fetchResponse);
+  const [data, setData] = useState<UseFetchData<T>>(fetchData);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [abortController, setAbortController] = useState<AbortController | null>(null);
@@ -37,13 +24,15 @@ const useFetch = <T>(url: string): UseFetchReturn<T> => {
   };
 
   useEffect(() => {
+    setData(new UseFetchData<T>())
     const controller = new AbortController();
     setAbortController(controller);
 
     fetch(url, { signal: controller.signal })
       .then(response => response.json())
       .then(jsonResponse => {
-        setData(jsonResponse as FetchResponse<T>);
+        data.setFetchData(jsonResponse as IUseFetchData<T>)
+        setData(data);
       })
       .catch(error => {
         if (error.name !== "AbortError") {
