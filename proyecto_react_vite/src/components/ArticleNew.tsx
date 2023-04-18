659839782bbaee2872cdf4b3004e8003f8f8ed51
Luis B <luis.beroiza@ottogroup.com>
2023-04-18 21:04:31 +0200

    Creando componente para boton de crear nuevos articulos

---
+++ proyecto_react_vite/src/components/ArticleNew.tsx
@@ -0,0 +1,10 @@
+const ArticleNew = (): JSX.Element => {
+  return (
+    <div id="nav-blog" className="sidebar-item">
+      <h3>Puedes hacer esto</h3>
+      <a href="" className="btn btn-success">Crear Articulo</a>
+    </div>
+  );
+}
+
+export default ArticleNew;
