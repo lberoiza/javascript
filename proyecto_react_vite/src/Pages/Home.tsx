f00de80bb60cf78109c2c7acbcdef34b6681b830
Luis B <luis.beroiza@ottogroup.com>
2023-04-18 14:02:01 +0200

    Usa Componente ArticleListLast en la pagina principal
    Este componente muesta la lista de los ultimos articulos

--- proyecto_react_vite/src/Pages/Home.tsx
+++ proyecto_react_vite/src/Pages/Home.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Slider from '../components/Slider';
 import Sidebar from '../components/Sidebar';
-import ArticleList from "../components/ArticleList";
+import ArticleListLast from '../components/ArticleListLast';
 
 class Home extends Component {
 
@@ -14,7 +14,7 @@ class Home extends Component {
           <section id="content">
             <h2 className="subheader">Últimos artículos</h2>
             {/* <!-- LISTADO ULTIMOS ARTICULOS ARTICULOS --> */}
-            <ArticleList nrOfArticlesToShow={3}></ArticleList>
+            <ArticleListLast></ArticleListLast>
           </section>
           <Sidebar></Sidebar>
         </div>
