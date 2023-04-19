c99800b7a7baab1d20ac5267ae0350e4fe4875cf
Luis B <luis.beroiza@ottogroup.com>
2023-04-19 11:34:18 +0200

    Agrega formulario al componente de creacion de articulos

--- proyecto_react_vite/src/components/ArticleNew.tsx
+++ proyecto_react_vite/src/components/ArticleNew.tsx
@@ -1,7 +1,27 @@
 export default function ArticleNew(): JSX.Element {
   return (
-    <div className="article-item">
-      <h1 className="subheader">Nuevo Articulo</h1>
+    <div className="new-article-form">
+      <form action="" className="mid-form">
+
+        <div className="form-group">
+          <label htmlFor="title">Agregue el titulo del artículo</label>
+          <input type="text" name="title" placeholder="Mi Articulo" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="content" >Agrege el contenido del artículo</label>
+          <textarea name="content" placeholder="este articulo trata de..."></textarea>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="imagen" >Agrege la imagen del articulo</label>
+          <input type="file" name="imagen" />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" className="btn btn-success" />
+        </div>
+      </form>
     </div>
   );
 }
