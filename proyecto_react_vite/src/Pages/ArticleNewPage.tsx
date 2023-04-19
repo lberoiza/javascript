d612026fc622b419a9e2308c8c7ea2d13194a084
Luis B <luis.beroiza@ottogroup.com>
2023-04-19 11:34:42 +0200

    Agrega Slider a la pagina de creacion de articulos

--- proyecto_react_vite/src/Pages/ArticleNewPage.tsx
+++ proyecto_react_vite/src/Pages/ArticleNewPage.tsx
@@ -1,9 +1,11 @@
 import ArticleNew from "../components/ArticleNew";
 import Sidebar from "../components/Sidebar";
+import Slider from "../components/Slider";
 
 export default function ArticleNewPage(): JSX.Element {
   return (
     <div id="create">
+      <Slider title="Nuevo Articulo"></Slider>
       <div className="center">
         <section id="content">
           <ArticleNew></ArticleNew>
