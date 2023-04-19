02d44f074be3c3f144a6a0eb9ff29ecadf5177d2
Luis B <luis.beroiza@ottogroup.com>
2023-04-19 23:37:48 +0200

    Agrega la funcionalidad para Crear un Articulo con imagen

--- proyecto_react_vite/src/components/ArticleNew.tsx
+++ proyecto_react_vite/src/components/ArticleNew.tsx
@@ -1,7 +1,55 @@
+import React, { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useForm from "../hooks/useForm";
+import FetchRequest from "../classes/FetchRequest";
+import { IUseFetchData } from '../classes/UseFetchData'
+import { ArticleResponse } from "../api/ApiArticle";
+import ApiConstant from "../api/ApiConstant";
+
+
+type ArticleFormFields = {
+  title: string,
+  content: string
+  imagen: File
+}
+
+
 export default function ArticleNew(): JSX.Element {
+  const navigation = useNavigate();
+  const [error, setError] = useState<string>('');
+  const { getData, validateStringFields } = useForm<ArticleFormFields>();
+
+  const formHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formDataObject = getData(event.target as HTMLFormElement);
+
+    try {
+      if (!validateStringFields(formDataObject)) throw new Error("Titulo o Contenido del Articulo vacio.");
+      if (formDataObject.imagen.name == '') throw new Error("No se ha Selecionado imagen para el Articulo");
+
+      const { promise } = FetchRequest.post<ArticleResponse>(ApiConstant.ARTICLE.POST_NEW_ARTICLE, formDataObject)
+      promise.then(wsResult => {
+        const id = wsResult.response?._id;
+        const form = new FormData();
+        form.append('imagen', formDataObject.imagen, formDataObject.imagen.name);
+
+        const { promise } = FetchRequest.postForm<ArticleResponse>(`${ApiConstant.ARTICLE.POST_ADD_IMAGE_TO_ARTICLE}/${id}`, form)
+        promise.then(wsResult => {
+          navigation(`/blog/article/${id}`);
+        });
+      });
+
+    } catch (error) {
+      console.log(error);
+      setError((error as Error).message)
+    }
+  }
+
+
   return (
     <div className="new-article-form">
-      <form className="mid-form">
+      {error && <div className="message-error">{error}</div>}
+      <form className="mid-form" onSubmit={formHandler}>
 
         <div className="form-group">
           <label htmlFor="title">Agregue el titulo del artículo</label>
@@ -15,7 +63,7 @@ export default function ArticleNew(): JSX.Element {
 
         <div className="form-group">
           <label htmlFor="imagen" >Agrege la imagen del articulo</label>
-          <input type="file" name="imagen" />
+          <input id="file-input" type="file" name="imagen" />
         </div>
 
         <div className="form-group">
