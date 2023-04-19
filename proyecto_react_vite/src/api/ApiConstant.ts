3f05bc7ea51132b08ee46273cc27d7c6b87f78f9
Luis B <luis.beroiza@ottogroup.com>
2023-04-19 23:33:58 +0200

    Agrega nuevas direcciones de la API-REST
    - crear articulos
    - agregar imagen a los articulos

--- proyecto_react_vite/src/api/ApiConstant.ts
+++ proyecto_react_vite/src/api/ApiConstant.ts
@@ -5,6 +5,8 @@ type ApiConstantProps = {
     readonly GET_BY_ID: string,
     readonly GET_ALL_ARTICLES: string,
     readonly GET_ALL_BY_SEARCH: string
+    readonly POST_NEW_ARTICLE: string
+    readonly POST_ADD_IMAGE_TO_ARTICLE: string
   },
   readonly IMAGE: {
     readonly GET_IMAGE_BY_NAME: string
@@ -16,7 +18,9 @@ const ApiConstant: ApiConstantProps = {
   ARTICLE: {
     GET_BY_ID: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article`,
     GET_ALL_ARTICLES: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/all`,
-    GET_ALL_BY_SEARCH: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/search`
+    GET_ALL_BY_SEARCH: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/search`,
+    POST_NEW_ARTICLE: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/save`,
+    POST_ADD_IMAGE_TO_ARTICLE: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/article/upload_image`
   },
   IMAGE: {
     GET_IMAGE_BY_NAME: `http://${Config.APP_API_HOST}:${Config.APP_PORT}${Config.APP_API_PREFIX}/image`
