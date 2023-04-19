7c3d2cc9a9cc6cf84dce532903ef46c1d8c74f3f
Luis B <luis.beroiza@ottogroup.com>
2023-04-19 13:14:09 +0200

    Renombra variable

--- proyecto_react_vite/src/components/Sidebar.tsx
+++ proyecto_react_vite/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import SearchArticleForm from "./SidebarSearchArticleForm";
+import SidebarSearchArticleForm from "./SidebarSearchArticleForm";
 import SidebarArticleNew from "./SidebarArticleNew";
 
 
@@ -11,7 +11,7 @@ const Sidebar = (props: SidebarProps): JSX.Element => {
   return (
     <aside id="sidebar">
       {props.isBlog && <SidebarArticleNew />}
-      <SearchArticleForm></SearchArticleForm>
+      <SidebarSearchArticleForm></SidebarSearchArticleForm>
     </aside>
   );
 }
