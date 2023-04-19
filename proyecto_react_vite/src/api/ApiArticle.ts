6e8b26457f7b2ffbeed4f0bc59063f2c71eecf40
Luis B <luis.beroiza@ottogroup.com>
2023-04-19 23:48:13 +0200

    Encapsula en una clase los metodos necesarios para la manipulacion de articulos

--- proyecto_react_vite/src/api/ApiArticle.ts
+++ proyecto_react_vite/src/api/ApiArticle.ts
@@ -12,29 +12,31 @@ export interface ArticleResponse {
   __v: number
 }
 
-
 export type ArticleFormFields = {
   title: string,
   content: string
   imagen: File
 }
 
+class ApiArticle {
+  public updateArticleImage(articleId: string, form: FormData, callback: (wsResult: IUseFetchData<ArticleResponse>) => void): void {
+    const { promise } = FetchRequest.postForm<ArticleResponse>(`${ApiConstant.ARTICLE.POST_ADD_IMAGE_TO_ARTICLE}/${articleId}`, form)
+    promise.then((wsResult) => callback(wsResult));
+  }
 
-export function updateArticleImage(articleId: string, form: FormData, callback: (wsResult: IUseFetchData<ArticleResponse>) => void): void {
-  const { promise } = FetchRequest.postForm<ArticleResponse>(`${ApiConstant.ARTICLE.POST_ADD_IMAGE_TO_ARTICLE}/${articleId}`, form)
-  promise.then((wsResult) => callback(wsResult));
+  public createArticle(formDataObject: ArticleFormFields, callback: (wsResult: IUseFetchData<ArticleResponse>) => void): void {
+    const { promise } = FetchRequest.post<ArticleResponse>(ApiConstant.ARTICLE.POST_NEW_ARTICLE, formDataObject)
+    promise.then((wsResult) => callback(wsResult));
+  }
+
+  public createCompleteArticle(formDataObject: ArticleFormFields, callback: (wsResult: IUseFetchData<ArticleResponse>) => void): void {
+    this.createArticle(formDataObject, (wsResult) => {
+      const id = wsResult.response?._id!;
+      const form = new FormData();
+      form.append('imagen', formDataObject.imagen, formDataObject.imagen.name);
+      this.updateArticleImage(id, form, callback);
+    });
+  }
 }
 
-export function createArticle(formDataObject: ArticleFormFields, callback: (wsResult: IUseFetchData<ArticleResponse>) => void): void {
-  const { promise } = FetchRequest.post<ArticleResponse>(ApiConstant.ARTICLE.POST_NEW_ARTICLE, formDataObject)
-  promise.then((wsResult) => callback(wsResult));
-}
-
-export function createCompleteArticle(formDataObject: ArticleFormFields, callback: (wsResult: IUseFetchData<ArticleResponse>) => void): void {
-  createArticle(formDataObject, (wsResult) => {
-    const id = wsResult.response?._id!;
-    const form = new FormData();
-    form.append('imagen', formDataObject.imagen, formDataObject.imagen.name);
-    updateArticleImage(id, form, callback);
-  });
-}
+export default new ApiArticle();
