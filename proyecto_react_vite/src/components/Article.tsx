2a623b2e408c8d8e02cac3a23097566b990d07ce
Luis B <luis.beroiza@ottogroup.com>
2023-04-20 11:27:02 +0200

    Implementa la funcionalidad del boton eliminar del componente articulo
    Cuando se presiona el boton eliminar se elimina el articulo.

--- proyecto_react_vite/src/components/Article.tsx
+++ proyecto_react_vite/src/components/Article.tsx
@@ -1,7 +1,10 @@
 import Dayjs from "../components/Dayjs";
 import ApiImage from "../api/ApiImage";
-import { ArticleResponse } from "../api/ApiArticle";
+import ApiArticle, { ArticleResponse } from "../api/ApiArticle";
 import ClearFix from "./ClearFix";
+import { NavigateFunction, NavigateOptions, To, useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import { SwalParams } from "sweetalert/typings/core";
 
 
 type ArticleProps = {
@@ -9,15 +12,56 @@ type ArticleProps = {
 }
 
 
+function deleteArticle(articleId: string, useNavigationHook: NavigateFunction): void {
+  const swalOptions = {
+    text: '',
+    className: 'swal-position-top-right',
+    timer: 2000,
+    icon: 'success'
+  }
+
+  try {
+    ApiArticle.deleteArticle(articleId, (wsResult) => {
+      swalOptions.text = `El Articulo: "${wsResult.response?.title}" fué eliminado correctamente.`;
+      useNavigationHook('/blog');
+      swal(swalOptions);
+    });
+  } catch (error) {
+    swalOptions.text = `El Articulo no pudo ser eliminado`;
+    swalOptions.icon = 'error';
+    swalOptions.timer = 0;
+    swal(swalOptions)
+  }
+}
+
+
+function confirmDeletion(articleId: string, useNavigationHook: NavigateFunction) {
+  swal({
+    title: "Está seguro que desea eliminar el articulo?",
+    text: "El borrado del articulo es permanente!",
+    icon: "warning",
+    buttons: ['cancelar', 'borrar'],
+    dangerMode: true,
+  })
+    .then((willDelete) => {
+      if (willDelete) {
+        deleteArticle(articleId, useNavigationHook);
+      }
+    });
+}
+
+
 export default function Article(props: ArticleProps): JSX.Element {
+  const navigation = useNavigate();
+
   return (
     <article id={props.article._id} className="article-item article-detail">
       <div className="image-wrap">
         <img src={ApiImage.getImageUrl(props.article.image)} alt="Article Image" />
       </div>
       <div className="article-button-bar">
-        <a href="#" className="btn btn-warning">Editar</a>
-        <a href="#" className="btn btn-danger">Eliminar</a>
+        <button className="btn btn-warning">Editar</button>
+        <button onClick={() => confirmDeletion(props.article._id, navigation)} className="btn btn-danger">Eliminar</button>
       </div>
       <ClearFix></ClearFix>
       <h1 className="subheader">{props.article.title}</h1>
