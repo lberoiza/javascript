33e8c5e4ef9761ea374339983b41978c88be8af2
Luis B <luis.beroiza@adesso.de>
2023-06-01 21:30:22 +0200

    Agrega clase active-class="navlink_active"
    se agrega la clase "navlink_active" a los links que están activos

--- proyecto_vue_vite/src/components/Menu.vue
+++ proyecto_vue_vite/src/components/Menu.vue
@@ -2,19 +2,21 @@
   <nav id="menu">
     <ul>
       <li>
-        <router-link  to="/">Home</router-link>
+        <router-link to="/" active-class="navlink_active">Home</router-link>
       </li>
       <li>
-        <router-link to="/blog">Blog</router-link>
+        <router-link to="/blog" active-class="navlink_active">Blog</router-link>
       </li>
       <li>
-        <router-link to="/form">Form</router-link>
+        <router-link to="/form" active-class="navlink_active">Form</router-link>
       </li>
-    <li>
-      <router-link to="/page1">Page 1</router-link>
-    </li>
-    <li>
-      <router-link :to="{name: 'pag', params: {msg: 'parametro por url desde el menu'}}">Page 2</router-link>
-    </li>
-  </ul>
-</nav></template>
+      <li>
+        <router-link to="/page1" active-class="navlink_active">Page 1</router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'pag', params: { msg: 'parametro por url desde el menu' } }"
+          active-class="navlink_active">Page 2</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
