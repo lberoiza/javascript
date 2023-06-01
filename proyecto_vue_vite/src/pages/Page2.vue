c3714cb7acd81f84a684066d9b68112891edb9b3
Luis B <luis.beroiza@adesso.de>
2023-06-01 21:47:33 +0200

    Agrega Boton para volver a Home

--- proyecto_vue_vite/src/pages/Page2.vue
+++ proyecto_vue_vite/src/pages/Page2.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 import Slider from '../components/Slider.vue';
 import Sidebar from '../components/Sidebar.vue';
 const route = useRoute();
+const router = useRouter();
 
 const msg = route.params.msg;
+
+const goHome = () => {
+  router.push('/home');
+}
 
 </script>
 
@@ -15,6 +20,7 @@ const msg = route.params.msg;
   <section id="content" class="page-2">
     <h2 class="subheader">Test Page 2</h2>
     <p v-if="msg">{{ msg }}</p>
+    <button @click="goHome">Go back to home</button>
   </section>
   <Sidebar></Sidebar>
 </template>
