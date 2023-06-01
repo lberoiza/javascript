0e8f8594119ca7d92e7cd6ca401e771fab5cd4d9
Luis B <luis.beroiza@adesso.de>
2023-06-01 21:34:47 +0200

    Cambia el texto del Boton del Slider

--- proyecto_vue_vite/src/components/Slider.vue
+++ proyecto_vue_vite/src/components/Slider.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="slider" :class="isHome ? 'slider-big' : 'slider-small'">
     <h1>{{ title }}</h1>
-    <router-link v-if="isHome" to="/blog" class="btn-white">Go to the Blog</router-link>
+    <router-link v-if="isHome" to="/blog" class="btn-white">Go to Blog</router-link>
   </div>
 </template>
 
