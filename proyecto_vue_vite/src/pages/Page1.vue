9b5b99d963409ad24c9c940c2adddd9735758608
Luis B <luis.beroiza@adesso.de>
2023-06-01 21:14:42 +0200

    Prueba Methodos del ciclo de vida y Reactividad

--- proyecto_vue_vite/src/pages/Page1.vue
+++ proyecto_vue_vite/src/pages/Page1.vue
@@ -2,6 +2,30 @@
 import Slider from '../components/Slider.vue';
 import Sidebar from '../components/Sidebar.vue';
 import MyComponent from '../components/MyComponent.vue';
+import { ref, onBeforeMount, onMounted, onUnmounted } from 'vue';
+
+const text = ref("Prop-Text from My Component");
+console.log("Page 1 - setup");
+
+// Metodos del Ciclo de vida
+onMounted(() => {
+  console.log("Page 1 - onMounted");
+});
+
+onBeforeMount(() => {
+  console.log("Page 1 - onBeforeMount");
+});
+
+onUnmounted(() => {
+  console.log("Page 1 - onUnmounted");
+});
+
+
+const click = (event: MouseEvent) => {
+  text.value = "The text has changed";
+}
+
+
 </script>
 
 
@@ -9,7 +33,8 @@ import MyComponent from '../components/MyComponent.vue';
   <Slider title='TestPage 1'></Slider>
   <section id="content" class="page-1">
     <h2 class="subheader">Test Page 1</h2>
-    <MyComponent msg="Prop-Text from My Component"></MyComponent>
+    <MyComponent :msg="text"></MyComponent>
+    <button @click="click">Button</button>
   </section>
   <Sidebar></Sidebar>
 </template>
