8ff1dff975010ca57662d48e1dfa8d27907ee1c1
Luis B <luis.beroiza@adesso.de>
2023-06-01 12:58:43 +0200

    Agrega seccion al MyComponent con id "component"

--- proyecto_vue_vite/src/components/MyComponent.vue
+++ proyecto_vue_vite/src/components/MyComponent.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="my-component">
-    <p>Hello World</p>
-    <p>{{ msg }}</p>
-  </div>
+  <section id="content" class="page-home">
+    <h2 class="subheader">My Component</h2>
+    <div class="my-component">
+      <p>Hello World</p>
+      <p>{{ msg }}</p>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
