c041382a17d46b5fde27fb803c72a4cdd6d50392
Luis B <luis.beroiza@adesso.de>
2023-06-03 01:12:05 +0200

    Agrega estado al componente Page2

--- proyecto_vue_vite/src/pages/Page2.vue
+++ proyecto_vue_vite/src/pages/Page2.vue
@@ -3,10 +3,20 @@ import { useRoute, useRouter } from "vue-router";
 
 import Slider from '../components/Slider.vue';
 import Sidebar from '../components/Sidebar.vue';
+import { ref } from "vue";
 const route = useRoute();
 const router = useRouter();
 
 const msg = route.params.msg;
+type Page2State = {
+  valor1: string
+};
+
+const testText = ref<Page2State>(
+  {
+    valor1: ''
+  });
+;
 
 const goHome = () => {
   router.push('/home');
@@ -20,6 +30,8 @@ const goHome = () => {
   <section id="content" class="page-2">
     <h2 class="subheader">Test Page 2</h2>
     <p v-if="msg">{{ msg }}</p>
+    <input type="text" name="text-test" id="text-test" v-model="testText.valor1">
+    <p class="show-text">The text in the input is: {{ testText.valor1 }}</p>
     <button @click="goHome">Go back to home</button>
   </section>
   <Sidebar></Sidebar>
