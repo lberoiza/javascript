ca2f9a6cf4f56d0a32a5448335c97dc67b7b3c9f
Luis B <luis.beroiza@adesso.de>
2023-06-13 09:10:03 +0200

    Envia datos desde hijo a padre
    para hacerlo se debe usar el hook defineEmits
    y se debe pasar al hijo el metodo del padre que será llamado usando @

--- proyecto_vue_vite/src/components/MyComponent.vue
+++ proyecto_vue_vite/src/components/MyComponent.vue
@@ -3,12 +3,32 @@
     <h2 class="subheader">My Component</h2>
     <div class="my-component">
       <p>Hello World</p>
-      <p>{{ msg }}</p>
+      <p>text of MyComponent: <strong>{{ msg }} </strong></p>
+      <div>
+        <p>Options from MyComponent</p>
+        <button @click="buttonClicked" name="Button1">Button 1</button>
+        <button @click="buttonClicked" name="Button2">Button 2</button>
+        <button @click="buttonClicked" name="Button3">Button 3</button>
+      </div>
+      <hr>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-type MyComponentType = { msg: string }
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['buttonHandler']);
+
+
+type MyComponentType = { msg: string}
 defineProps<MyComponentType>()
+
+
+const buttonClicked = (event: MouseEvent) => {
+  const target = event.target as HTMLButtonElement;
+  const name = target.getAttribute('name');
+  emit('buttonHandler', name)
+}
+
 </script>
--- proyecto_vue_vite/src/pages/Page1.vue
+++ proyecto_vue_vite/src/pages/Page1.vue
@@ -4,7 +4,8 @@ import Sidebar from '../components/Sidebar.vue';
 import MyComponent from '../components/MyComponent.vue';
 import { ref, onBeforeMount, onMounted, onUnmounted, computed } from 'vue';
 
-const text = ref("Prop-Text from My Component");
+const text = ref<string>("Prop-Text from My Component");
+const buttonClicked = ref<string>();
 console.log("Page 1 - setup");
 
 // Metodos del Ciclo de vida
@@ -21,8 +22,12 @@ onUnmounted(() => {
 });
 
 
+const buttonClickedFunction = (butonName: string) => {
+  buttonClicked.value = butonName;
+}
+
 const click = () => {
-  text.value = "The text has changed";
+  text.value = "The text has changed from Parent component";
 };
 
 
@@ -38,7 +43,10 @@ const computedValue = computed(() => {
   <Slider title='TestPage 1'></Slider>
   <section id="content" class="page-1">
     <h2 class="subheader">Test Page 1</h2>
-    <MyComponent :msg="text"></MyComponent>
+    <h4 v-if="buttonClicked">Button Clicked: {{ buttonClicked }}</h4>
+    <MyComponent :msg="text" @buttonHandler="buttonClickedFunction" ></MyComponent>
+    <br>
+    <span>Click to change the text of MyComponent from Parent </span>
     <button @click="click">Change Text</button>
     <div>
       <p>Print a computed value:</p>
