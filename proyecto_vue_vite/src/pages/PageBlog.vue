a758011e25014cbb522d4a32d5ca5fa854db621c
Luis B <luis.beroiza@adesso.de>
2023-06-16 11:26:47 +0200

    Agrega Pagina de Blog

--- proyecto_vue_vite/src/pages/PageBlog.vue
+++ proyecto_vue_vite/src/pages/PageBlog.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import Slider from '../components/Slider.vue';
 import Sidebar from '../components/Sidebar.vue';
+import ArticleList from '../components/ArticleList.vue';
 </script>
 
 
 <template>
   <Slider title='Blog'></Slider>
   <section id="content" class="page-blog">
+    <h2 class="subheader">List of Articles</h2>
+    <ArticleList></ArticleList>
   </section>
   <Sidebar isBlog></Sidebar>
 </template>
