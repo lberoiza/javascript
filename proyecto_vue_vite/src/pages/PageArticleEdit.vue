f0af5e6e0c2947807210dcaa062f731b438610e4
Luis B <luis.beroiza@adesso.de>
2023-06-28 13:38:48 +0200

    Agrega pagina para editar articulo

--- proyecto_vue_vite/src/pages/PageArticleEdit.vue
+++ proyecto_vue_vite/src/pages/PageArticleEdit.vue
@@ -1,14 +1,47 @@
 <template>
-  <Slider title='Create new Article'></Slider>
-  <section id="content" class="page-article-new">
-    <h2 class="subheader">Details of the Article</h2>
-    <ArticleForm></ArticleForm>
+  <section id="content" class="page-article-edit">
+    <Loading v-if="loading"></Loading>
+    <div v-else>
+      <Error v-if="article.hasErrors()" :errors="article.errorMessages"></Error>
+      <div v-else class="article-container">
+        <ArticleForm v-if="article.hasResponse()" :article="article.response!"></ArticleForm>
+        <NoResults v-else></NoResults>
+      </div>
+    </div>
   </section>
   <Sidebar></Sidebar>
 </template>
 
 <script setup lang="ts">
-import Slider from '../components/Slider.vue';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
 import Sidebar from '../components/Sidebar.vue';
 import ArticleForm from '../components/ArticleForm.vue';
+import ApiArticle, { ArticleResponse } from '../api/ApiArticle';
+import UseFetchData from '../classes/UseFetchData';
+import Error from '../components/Error.vue';
+import Loading from '../components/Loading.vue';
+import NoResults from '../components/NoResults.vue';
+
+
+const articleId = useRoute().params.articleId as string;
+const article = ref<UseFetchData<ArticleResponse>>(new UseFetchData<ArticleResponse>());
+const loading = ref<boolean>(true);
+
+const apiArticle = ApiArticle.getArticleById(articleId);
+
+onMounted(() => {
+  apiArticle.promise.then(wsResponse => {
+    loading.value = false;
+    article.value = new UseFetchData<ArticleResponse>().setFetchData(wsResponse);
+  });
+});
+
+onUnmounted(() => {
+  if (apiArticle.abortController) {
+    apiArticle.abortController.abort()
+  }
+
+});
+
 </script>
