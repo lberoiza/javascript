a65259e9c0d2ca84ba48097150cf6dc26fc82ebf
Luis B <luis.beroiza@adesso.de>
2024-02-07 21:47:43 +0100

    cambia el componente de clase a componente funcional

--- proyecto_react_vite/src/Pages/Home.tsx
+++ proyecto_react_vite/src/Pages/Home.tsx
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-
 import Slider from '../components/Slider';
 import Sidebar from '../components/Sidebar';
 import ArticleListLast from '../components/ArticleListLast';
 
-class Home extends Component {
-
-  public render(): JSX.Element {
-    return (
-      <>
-        <Slider isHome title='Bienvenido al master der ReactJS'></Slider>
-        <section id="content" className="page-home">
-          <h2 className="subheader">Últimos artículos</h2>
-          {/* <!-- LISTADO ULTIMOS ARTICULOS ARTICULOS --> */}
-          <ArticleListLast></ArticleListLast>
-        </section>
-        <Sidebar></Sidebar>
-      </>
-    );
-  }
+const Home = () => {
+  return (
+    <>
+      <Slider isHome title="Bienvenido al master der ReactJS"></Slider>
+      <section id="content" className="page-home">
+        <h2 className="subheader">Últimos artículos</h2>
+        {/* <!-- LISTADO ULTIMOS ARTICULOS ARTICULOS --> */}
+        <ArticleListLast></ArticleListLast>
+      </section>
+      <Sidebar></Sidebar>
+    </>
+  );
 }
+
 
 export default Home;
