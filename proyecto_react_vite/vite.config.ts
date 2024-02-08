3bad0613982909d3c0b82a97af12393fa46933c8
Luis B <luis.beroiza@adesso.de>
2024-02-08 20:53:18 +0100

    Agrega alias para todas las carpetas del proyecto

--- proyecto_react_vite/vite.config.ts
+++ proyecto_react_vite/vite.config.ts
@@ -8,8 +8,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "./src/"),
+      'api': path.resolve(__dirname, "./src/api/"),
+      'assets': path.resolve(__dirname, "./src/assets/"),
+      'classes': path.resolve(__dirname, "./src/classes/"),
       'components': path.resolve(__dirname, "./src/components/"),
+      'config': path.resolve(__dirname, "./src/config/"),
+      'hooks': path.resolve(__dirname, "./src/hooks/"),
+      'models': path.resolve(__dirname, "./src/models/"),
       'pages': path.resolve(__dirname, "./src/pages/"),
+      'store': path.resolve(__dirname, "./src/redux/")
     },
   }
 })
