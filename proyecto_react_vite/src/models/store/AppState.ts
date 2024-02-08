3c4bb663b4c136c849337dc9908c7fe6deea50f0
Luis B <luis.beroiza@adesso.de>
2024-02-08 13:30:54 +0100

    elimina clase que representa el estado de la app
    esa clase se genera automaticamente desde "src\redux\store.ts"

--- proyecto_react_vite/src/models/store/AppState.ts
+++
@@ -1,7 +0,0 @@
-import { UserState } from "@/models/store/user/user-state";
-import { ArticleState } from "@/models/store/user/article-state";
-
-export interface AppState{
-  user: UserState,
-  articles: ArticleState
-}
