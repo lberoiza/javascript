82173979b8f44ddf7343e702565cc8e564e441d5
Luis B <luis.beroiza@adesso.de>
2024-02-09 21:29:14 +0100

    Agrega punto y coma al final de la linea

--- proyecto_react_vite/src/hooks/useNavigateWithTransitions.ts
+++ proyecto_react_vite/src/hooks/useNavigateWithTransitions.ts
@@ -2,7 +2,7 @@ import { flushSync } from "react-dom";
 import { useNavigate } from "react-router-dom";
 
 export const useNavigateWithTransitions = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (href: string) => {
     if (!document.startViewTransition) {
       navigate(href);
