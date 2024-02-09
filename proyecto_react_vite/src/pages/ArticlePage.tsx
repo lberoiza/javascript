67c212045d45d9893702963178fe99a305d544c8
Luis B <luis.beroiza@adesso.de>
2024-02-09 21:51:12 +0100

    Busca el articulo seleccionado del store para acelerar la carga
    de esta forma se puede usar la transicion de paginas
    sino está en el store se carga desde la api

--- proyecto_react_vite/src/pages/ArticlePage.tsx
+++ proyecto_react_vite/src/pages/ArticlePage.tsx
@@ -5,28 +5,48 @@ import NotFound from "@/pages/NotFound";
 import Sidebar from "@/components/Sidebar";
 import { useFetchArticleById } from "@/hooks/useFetchArticleById";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AppState } from "@/store/store";
+import { ArticleResponse } from "@/models/ArticleResponse.model";
 
 type ArticleUrlParams = {
   id: string;
 }
 
-const ArticlePage = (): JSX.Element => {
-  const {id} = useParams<ArticleUrlParams>();
-  if (id === undefined) return (<NotFound></NotFound>)
-  const { article, loading, error } = useFetchArticleById(id);
+type RenderProperties = {
+  article?: ArticleResponse;
+  loading?: boolean;
+  error?: boolean;
+}
 
+const renderPage = (renderProps: RenderProperties) => {
+  const {article, loading, error} = renderProps;
   return (
     <>
       <section id="content" className="page-article">
         <div id="article-container">
           {loading && <Loading></Loading>}
           {error && <Error></Error>}
-          {article && <Article article={article!}></Article>}
+          {article && <Article article={article}></Article>}
         </div>
       </section>
       <Sidebar isBlog></Sidebar>
     </>
   );
+}
+
+
+const ArticlePage = (): JSX.Element => {
+  const {id} = useParams<ArticleUrlParams>();
+  if (id === undefined) return (<NotFound></NotFound>)
+
+  const storedArticle = useSelector((state: AppState) => state.articles.current);
+  if (storedArticle) {
+    return renderPage({article: storedArticle});
+  }
+
+  const {article, loading, error} = useFetchArticleById(id);
+  return renderPage({article, loading, error});
 }
 
 export default ArticlePage;
