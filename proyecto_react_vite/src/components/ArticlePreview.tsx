ded514bb90f8c839105a1d9c6ce8976f48f0108c
Luis B <luis.beroiza@adesso.de>
2024-02-09 21:30:45 +0100

    Al clickear un articulo para verlo en detalle se guarda en el store
    cuando se quiere ver el detalle de un articulo se guarda en el store
    para que no se deba ejecutar un request a la api

--- proyecto_react_vite/src/components/ArticlePreview.tsx
+++ proyecto_react_vite/src/components/ArticlePreview.tsx
@@ -1,34 +1,44 @@
 import React, { useState, MouseEvent } from "react";
 import ApiImage from "@/api/ApiImage";
 import Dayjs from "@/components/Dayjs";
-import LinkWithTransition from "@/components/LinkWithTransition";
 import { ArticleResponse } from '@/models/ArticleResponse.model';
 import { Link } from 'react-router-dom';
+import { useNavigateWithTransitions } from "@/hooks/useNavigateWithTransitions";
+import { useDispatch } from "react-redux";
+import { setCurrentArticle } from "@/store/article/articles.actions";
 
 export interface ArticlePreviewProps {
   article: ArticleResponse;
+
   follow(article: ArticleResponse): void;
 };
 
-const ArticlePreview: React.FC<ArticlePreviewProps> = ({article, follow }) => {
-  const [btnSeguir, setBtnSeguir] = useState({ disabled: false });
+const ArticlePreview: React.FC<ArticlePreviewProps> = ({article, follow}) => {
+  const navigate = useNavigateWithTransitions();
+  const dispatch = useDispatch();
+
+  const [btnSeguir, setBtnSeguir] = useState({disabled: false});
 
   const callFollow = (event: MouseEvent<HTMLButtonElement>): void => {
-    setBtnSeguir({ disabled: true });
+    setBtnSeguir({disabled: true});
     follow(article);
+  }
+
+  const gotToArticlePage = () => {
+    dispatch(setCurrentArticle(article));
+    navigate('/blog/article/' + article._id);
   }
 
   return (
     <article id={article._id} className="article-item">
       <div className="image-wrap"
+           onClick={gotToArticlePage}
            style={{viewTransitionName: `article${article._id}`}}
       >
-        <LinkWithTransition href={'/blog/article/' + article._id}>
-          <img className="preview"
-               src={ApiImage.getImageUrl(article.image)}
-               alt="Article Image"
-          />
-        </LinkWithTransition>
+        <img className="preview"
+             src={ApiImage.getImageUrl(article.image)}
+             alt="Article Image"
+        />
       </div>
       <button className="btn-follow star-button" onClick={callFollow} disabled={btnSeguir.disabled}>
         <span className="star-icon"></span>
