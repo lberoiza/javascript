d2a53186e611221e8124c54d7d9aedfcc08a98f7
Luis B <luis.beroiza@adesso.de>
2024-02-09 22:33:40 +0100

    Obtiene los datos del Articulo a editar desde el store
    para acelerar la carga obtiene los datos del store,
    de no encontrarlos los va a buscar a la api rest

--- proyecto_react_vite/src/pages/PageEditArticle.tsx
+++ proyecto_react_vite/src/pages/PageEditArticle.tsx
@@ -1,26 +1,50 @@
-import ApiConstant from "../api/ApiConstant";
 import ArticleEdit from "@/components/ArticleEdit";
 import Error from "@/components/Error";
 import Loading from "@/components/Loading";
 import Sidebar from "@/components/Sidebar";
 import Slider from "@/components/Slider";
-import useFetch from "@/hooks/useFetch";
 import { ArticleResponse } from "@/models/ArticleResponse.model";
 import { useParams } from "react-router-dom";
+import NotFound from "@/pages/NotFound";
+import { useSelector } from "react-redux";
+import { AppState } from "@/store/store";
+import { useFetchArticleById } from "@/hooks/useFetchArticleById";
 
-export default function PageEditArticle(): JSX.Element {
-  const { id } = useParams();
-  const { data, error, loading } = useFetch<ArticleResponse>(`${ApiConstant.ARTICLE.GET_BY_ID}/${id}`);
+type ArticleUrlParams = {
+  id: string;
+}
 
+type RenderProperties = {
+  article?: ArticleResponse;
+  loading?: boolean;
+  error?: boolean;
+}
+
+const renderPage = (renderProps: RenderProperties) => {
+  const {article, loading, error} = renderProps;
   return (
     <>
       <Slider title="Edita Articulo"></Slider>
       <section id="content" className="page-edit-article">
         {loading && <Loading></Loading>}
         {error && <Error></Error>}
-        {data.hasResponse() && <ArticleEdit article={data.response!}></ArticleEdit>}
+        {article && <ArticleEdit article={article}></ArticleEdit>}
       </section>
       <Sidebar></Sidebar>
     </>
   );
+}
+
+
+export default function PageEditArticle(): JSX.Element {
+  const {id} = useParams<ArticleUrlParams>();
+  if (id === undefined) return (<NotFound></NotFound>)
+
+  const storedArticle = useSelector((state: AppState) => state.articles.current);
+  if (storedArticle) {
+    return renderPage({article: storedArticle});
+  }
+
+  const {article, loading, error} = useFetchArticleById(id);
+  return renderPage({article, loading, error});
 }
