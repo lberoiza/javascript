7305c2fe506f6a22c10b49d4c3bdf2cd2ffd27e9
Luis B <luis.beroiza@adesso.de>
2024-02-09 17:37:16 +0100

    Mejora la carga de articulos
    debido a que la vista de un articulo busca en el store
    en la variable all.
    La lista de ultimos articulos se agregan si la lista está vacia

--- proyecto_react_vite/src/components/ArticleListLast.tsx
+++ proyecto_react_vite/src/components/ArticleListLast.tsx
@@ -4,13 +4,13 @@ import Config from "@/config/Config";
 import { AppState } from "@/store/store";
 import { ArticleResponse } from "@/models/ArticleResponse.model";
 import { IUseFetchData } from "@/classes/UseFetchData";
-import { setLastArticles } from "@/store/article/articles.actions";
+import { setAllArticles, setLastArticles } from "@/store/article/articles.actions";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 
 const ArticleListLast = (): JSX.Element => {
-  const lastArticles = useSelector((state: AppState) => state.articles.last);
+  const articles = useSelector((state: AppState) => state.articles);
   const dispatch = useDispatch();
 
   const lastArticlesUrl = ApiConstant.ARTICLE.GET_ALL_ARTICLES + '/' + Config.DEFAULT_NR_OF_ARTICLES_HOMEPAGE;
@@ -21,13 +21,16 @@ const ArticleListLast = (): JSX.Element => {
       .then((data: IUseFetchData<ArticleResponse>) => {
         if (data.isSuccessful && data.response) {
           dispatch(setLastArticles(data.response));
+          if(articles.all.length === 0) {
+            dispatch(setAllArticles(data.response));
+          }
         }
       })
   }, []);
 
 
   return (
-    <ArticleList articles={lastArticles}></ArticleList>
+    <ArticleList articles={articles.last}></ArticleList>
   );
 }
 
