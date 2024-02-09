b6525aee363183a8b5b6c451dda78e32f2ea4175
Luis B <luis.beroiza@adesso.de>
2024-02-09 17:43:33 +0100

    Agrega a los links el uso Links con transiciones

--- proyecto_react_vite/src/components/Menu.tsx
+++ proyecto_react_vite/src/components/Menu.tsx
@@ -1,29 +1,17 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import LinkWithTransition from "@/components/LinkWithTransition";
 
-
-
-class Menu extends Component {
-
-  classNameMethod(arg: {isActive: boolean, isPending: boolean }) : string {
-    return arg.isActive ? "navlink_active" : ""
-  }
-  
-
-
-  public render(): JSX.Element {
-    return (
-      < nav id="menu" >
-        <ul>
-          <li><NavLink className={this.classNameMethod} to="/">Inicio</NavLink></li>
-          <li><NavLink className={this.classNameMethod} to="blog">Blog</NavLink></li>
-          <li><NavLink className={this.classNameMethod} to="formulario">Formulario</NavLink></li>
-          <li><NavLink className={this.classNameMethod} to="pagina1/funcional/receta">Pagina 1</NavLink></li>
-          <li><NavLink className={this.classNameMethod} to="pagina2">Pagina 2</NavLink></li>
-        </ul>
-      </nav >
-    );
-  }
+const Menu = (): JSX.Element => {
+  return (
+    < nav id="menu">
+      <ul>
+        <li><LinkWithTransition href="/">Inicio</LinkWithTransition></li>
+        <li><LinkWithTransition href="/blog">Blog</LinkWithTransition></li>
+        <li><LinkWithTransition href="/formulario">Formulario</LinkWithTransition></li>
+        <li><LinkWithTransition href="/pagina1/funcional/receta">Pagina 1</LinkWithTransition></li>
+        <li><LinkWithTransition href="/pagina2">Pagina 2</LinkWithTransition></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Menu;
