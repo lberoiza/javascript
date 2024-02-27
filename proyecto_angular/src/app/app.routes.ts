a0c23489fcb0bfb6e3903aecf47ac11db6a08195
Luis B <luis.beroiza@adesso.de>
2024-02-27 09:25:25 +0100

    Agrega ruta hacia blog para ver los articulos publicados

--- proyecto_angular/src/app/app.routes.ts
+++ proyecto_angular/src/app/app.routes.ts
@@ -4,6 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('@/pages/home/home.component').then(chunk => chunk.HomeComponent)
-  }
-
+  },
+  {
+    path: 'blog',
+    loadComponent: () => import('@/pages/blog/blog.component').then(chunk => chunk.BlogComponent)
+  },
 ];
