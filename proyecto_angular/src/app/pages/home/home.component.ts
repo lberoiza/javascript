9fb18c4f7547a4da60b91861642e43a6e50a1e04
Luis B <luis.beroiza@adesso.de>
2024-02-27 09:39:49 +0100

    Fix, cambia la funcion splice por slice
    splice modificaba el array, se necesitaba solamente una copia

--- proyecto_angular/src/app/pages/home/home.component.ts
+++ proyecto_angular/src/app/pages/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit{
   protected articles: Article[] = []
 
   ngOnInit(): void {
-    this.articles = allArticles.splice(0, 3);
+    this.articles = allArticles.slice(0, 3);
   }
 
 }
