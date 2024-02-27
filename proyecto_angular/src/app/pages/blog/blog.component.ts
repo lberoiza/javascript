48ea7eadceb3cffa5edab7a3bb138652bd161f31
Luis B <luis.beroiza@adesso.de>
2024-02-27 10:26:36 +0100

    muesta todos los articulos disponibles

--- proyecto_angular/src/app/pages/blog/blog.component.ts
+++ proyecto_angular/src/app/pages/blog/blog.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from "@/models/Article.model";
+import { allArticles } from "../../../assets/data";
+import { ArticlePreviewComponent } from "@/components/article-preview/article-preview.component";
+import { PageContentComponent } from "@/components/page-content/page-content.component";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [
+    ArticlePreviewComponent,
+    PageContentComponent
+  ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit{
+
+  protected articles: Article[] = [];
+
+  ngOnInit(): void {
+    this.articles = allArticles;
+  }
 
 }
