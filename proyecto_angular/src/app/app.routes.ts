66030da2d6287552a799fc7d8527c6ac25cc25fb
Luis B <luis.beroiza@adesso.de>
2024-02-27 12:24:05 +0100

    Agrega la ruta para mostrar un artículo por ID

--- proyecto_angular/src/app/app.routes.ts
+++ proyecto_angular/src/app/app.routes.ts
@@ -3,10 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('@/pages/home/home.component').then(chunk => chunk.HomeComponent)
+    loadComponent: () => import('@/pages/home/home.component')
+      .then(chunk => chunk.HomeComponent)
   },
   {
     path: 'blog',
-    loadComponent: () => import('@/pages/blog/blog.component').then(chunk => chunk.BlogComponent)
+    loadComponent: () => import('@/pages/blog/blog.component')
+      .then(chunk => chunk.BlogComponent)
   },
+  {
+    path: 'blog/article/:id',
+    loadComponent: () => import('@/pages/article/article.component')
+      .then(chunk => chunk.ArticleComponent)
+  },
+
 ];
