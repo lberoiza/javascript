ea9c662ae64baa26c9167e54bddb4aac5a62773e
Luis B <luis.beroiza@adesso.de>
2024-02-27 09:25:06 +0100

    Estilando Links de navegacion usando css
    se le agrega la clase nav-menu__active a los links activos
    usando routerLinkActive

--- proyecto_angular/src/app/components/nav-menu/nav-menu.component.ts
+++ proyecto_angular/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css'
