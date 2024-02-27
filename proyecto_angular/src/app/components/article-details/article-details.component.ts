2c2fee36df62cb6f800f92ece3f8e98b53b92480
Luis B <luis.beroiza@adesso.de>
2024-02-27 21:50:07 +0100

    Agrega la funcionalidad a los 2 botones Editar y Eliminar
    Boton editar redirige al formulario de edicion
    boton eliminar muestra dialogo de confirmacion y si es confirmado
    se elimina el articulo

--- proyecto_angular/src/app/components/article-details/article-details.component.ts
+++ proyecto_angular/src/app/components/article-details/article-details.component.ts
@@ -3,12 +3,16 @@ import { Article } from "@/models/Article.model";
 import { allArticles } from "../../../assets/data";
 import { getImageUrl } from "@/libs/ImageUtils";
 import { DayjsComponent } from "@/components/dayjs/dayjs.component";
+import { Router, RouterLink } from "@angular/router";
+import { AlertMessage, AlertService } from "@/services/alerts/alert.service";
+import { ApiArticlesService } from "@/services/api-articles/api-articles.service";
 
 @Component({
   selector: 'app-article-details',
   standalone: true,
   imports: [
-    DayjsComponent
+    DayjsComponent,
+    RouterLink
   ],
   templateUrl: './article-details.component.html',
   styleUrl: './article-details.component.css'
@@ -19,4 +23,48 @@ export class ArticleDetailsComponent {
   @Input()
   article: Article = allArticles[0];
 
+  constructor(
+    private alert: AlertService,
+    private apiArticleService: ApiArticlesService,
+    private router: Router
+  ) {
+  }
+
+
+  protected confirmDeleteArticle = (articleId: string) => {
+
+    const message: AlertMessage = {
+      title: 'The deletion is permanent!',
+      content: 'Are you sure, that you want to delete the Article?'
+    }
+
+    this.alert.confirmDialog(message)
+      .then((willDelete) => {
+        if (willDelete) {
+          this.deleteArticle(articleId);
+        }
+      });
+  }
+
+  private deleteArticle = (articleId: string) => {
+    const alertMessage: AlertMessage = {
+      title: '',
+      content: ''
+    }
+
+    this.apiArticleService.deleteArticle(articleId)
+      .subscribe(apiResult => {
+        if (apiResult.isSuccessful) {
+          alertMessage.title = `Successfully deleted`;
+          alertMessage.content = `The Article: "${apiResult.response?.title}" was successfully deleted.`;
+          this.router.navigate(['/blog']).then(() => {
+            this.alert.showSuccess(alertMessage);
+          })
+        } else {
+          alertMessage.title = `Error`;
+          alertMessage.content = `The Article could not be eliminated`;
+          this.alert.showError(alertMessage);
+        }
+      })
+  }
 }
