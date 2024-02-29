0734cae04c6335d02cc375dd6c4cc689284a4403
Luis B <luis.beroiza@adesso.de>
2024-02-29 17:50:47 +0100

    Agrega @ngrx/store-devtools@latest
    Sirve para porder ver en modo de desarrollo el store
    
    ng add @ngrx/store-devtools@latest

--- proyecto_angular/src/app/app.config.ts
+++ proyecto_angular/src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from "@angular/common/http";
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
-    provideStore()
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
