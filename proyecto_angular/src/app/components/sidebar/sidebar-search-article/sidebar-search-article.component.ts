df6871f4cb8df10c059ca338e27366ab65d5ef9c
Luis B <luis.beroiza@adesso.de>
2024-02-29 10:47:28 +0100

    Implementa la busqueda de articulos en el formulario de busqueda
    redirecciona a la pagina de busqueda pasando por la url el string
    de busqueda

--- proyecto_angular/src/app/components/sidebar/sidebar-search-article/sidebar-search-article.component.ts
+++ proyecto_angular/src/app/components/sidebar/sidebar-search-article/sidebar-search-article.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-sidebar-search-article',
@@ -16,5 +17,24 @@ import { FormsModule } from "@angular/forms";
 export class SidebarSearchArticleComponent {
 
   protected searchStr: string = '';
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  private isFormValid(): boolean {
+    return this.searchStr.trim().length > 0;
+  }
+
+  protected search(mySearchForm: NgForm): void {
+    if(this.isFormValid()) {
+      this.router.navigate(['/blog/search', this.searchStr]).then();
+    }
+  }
+
+  shouldInputBeSelected() {
+    return this.searchStr.length > 0;
+  }
 
 }
