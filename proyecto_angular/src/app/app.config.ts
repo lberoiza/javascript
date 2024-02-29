e672a4cfca38662388572c4905994ce6fa36ef94
Luis B <luis.beroiza@adesso.de>
2024-02-29 13:22:27 +0100

    Instalando NgRx
    ng add @ngrx/store@latest

--- proyecto_angular/src/app/app.config.ts
+++ proyecto_angular/src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from "@angular/common/http";
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
-  ]
+    provideStore()
+]
 };
