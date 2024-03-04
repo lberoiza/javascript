013945c37eb4ab67fa0821f450c2186c3035932d
Luis B <luis.beroiza@adesso.de>
2024-03-04 15:15:59 +0100

    Traduciendo al ingles

--- proyecto_react_vite/src/pages/Home.tsx
+++ proyecto_react_vite/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Slider from '@/components/Slider';
 const Home = () => {
   return (
     <>
-      <Slider isHome title="Bienvenido al master der ReactJS"></Slider>
+      <Slider isHome title="Welcome This Blog written in ReactJs"></Slider>
       <section id="content" className="page-home">
         <h2 className="subheader">Last published Articles</h2>
         {/* <!-- LISTADO ULTIMOS ARTICULOS ARTICULOS --> */}
